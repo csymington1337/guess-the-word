0b79ae91e349f04fa6c8dcdb23ec7dbd7a839b3f
csymington1337 <symingtonchristina@gmail.com>
2021-07-15 23:42:58 -0600

    Added conditionals to validate user input.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const guessedLetters = document.querySelector(".guessed-letters");
+const guessedLettersElement = document.querySelector(".guessed-letters");
 const guessButton = document.querySelector(".guess");
 const letterInput = document.querySelector(".letter");
 const wordInProgress = document.querySelector(".word-in-progress");
@@ -6,7 +6,9 @@ const remainingGuesses = document.querySelector(".remaining");
 const remainingGuessesSpan = document.querySelector("span");
 const message = document.querySelector(".message");
 const playAgainButton = document.querySelector(".play-again");
+
 const word = "magnolia";
+const guessedLetters = [];
 
 const updatePlaceholder = function (word) {
     const placeholderLetters = [];
@@ -21,7 +23,36 @@ updatePlaceholder(word);
 
 guessButton.addEventListener("click", function (e) {
     e.preventDefault();
-    const guess = userInput.value;
-    console.log(guess);
-    userInput.value="";
+    message.innerText = "";
+    const guess = letterInput.value;
+    const validGuess = validateInput(guess);
+
+    if (validGuess) {
+        makeGuess(guess);
+    }
+
+    letterInput.value = "";
 });
+
+const validateInput = function (input) {
+    const acceptedLetter = /[a-zA-Z]/;
+    if (input.length === 0) {
+        message.innerText = "Oops, you didn't enter anything! Try a letter from A to Z.";
+    } else if (input.length > 1) {
+        message.innerText = "I like the enthusiasm, but that's too many letters. Try again!";
+    } else if (!input.match(acceptedLetter)) {
+        message.innerText = "Well, that was very creative... How about just a regular letter now?";
+    } else {
+        return input;
+    }
+};
+
+const makeGuess = function (guess) {
+    guess = guess.toUpperCase();
+    if (guessedLetters.includes()) {
+        message.innerText = "Is there an echo in here? You've already guessed that letter!";
+    } else {
+        guessedLetters.push(guess);
+        console.log(guessedLetters);
+    }
+};
